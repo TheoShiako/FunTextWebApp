118d8ee146a59b3abdd723ba2708e0f1c24ae53e
TheoShiako <151511315+TheoShiako@users.noreply.github.com>
2025-01-09 10:17:18 +0000

    Add project files.

---
+++ FunTextMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FunTextMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
